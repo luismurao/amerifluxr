3f5c63c6f672106d279a61f1a8bb4ae867a6ccc8
Koen Hufkens <khufkens@Pandora.local>
2016-03-08 00:24:17 -0500

    resolving errors

--- R/amerifluxr.r
+++ R/amerifluxr.r
@@ -3,9 +3,9 @@
 #' @export
 #' @examples
 #' # outputting TRUE or FALSE
-#' ameriflux.gui()
+#' amerifluxr()
 
-ameriflux.gui <- function(){
+amerifluxr <- function(){
   appDir = sprintf("%s/R/inst/shiny/ameriflux_explorer",path.package("amerifluxr"))
   #appDir='/data/Dropbox/Research_Projects/code_repository/bitbucket/amerifluxr/R/inst/shiny/ameriflux_explorer'
   shiny::runApp(appDir, display.mode = "normal",launch.browser=TRUE)
